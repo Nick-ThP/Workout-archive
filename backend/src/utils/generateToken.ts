e049f17aeb63d32beb0e122521c56a3f287d13e3
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2023-11-04 00:07:47 +0100

    created testable routes in thunder client

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { User } from './types'
 
 export const generateToken = (user: User) => {
-	return jwt.sign({ user }, process.env.JWT_SECRET as string, {
+	return jwt.sign(user, process.env.JWT_SECRET as string, {
 		expiresIn: '30d',
 	})
 }
